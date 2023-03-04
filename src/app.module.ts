8ada7154a29c049a46ceebab5edf6a0ae21ec074
Kadotchi <kadotchi724@gmail.com>
2023-03-04 16:53:33 +0900

    ✨ Prismaの設定とその他インポート

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, TodoModule, PrismaModule],
+  imports: [
+    // appモジュールでグローバルをtrueにして
+    // インポートすることで個々のモジュールでのインポートが必要なくなる
+    // ここでは環境変数を使うためにインポートする。
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    TodoModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// クラスバリデーションを有効化するために必要
+import { ValidationPipe } from '@nestjs/common';
+// Requestのデータ型
+import { Request } from 'express';
+// クライアントのデータからCookieを取り出すために必要
+import * as cookieParser from 'cookie-parser';
+// csrf対策用のトークンを生成するのに必要
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // DTOとクラスバリデーションを有効化
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // フロント側と通信するために必要な設定
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  // フロントエンドから受け取ったCookieを解析できるようにする
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
