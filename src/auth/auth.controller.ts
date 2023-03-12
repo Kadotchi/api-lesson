41646fc06108630b6f47870a62deddaf335329d8
Kadotchi <kadotchi724@gmail.com>
2023-03-12 21:21:01 +0900

    ✨ 認証サービスを実装

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Req,
+  Get,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Csrf, Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // エンドポイントの指定
+  // @Body でリクエストボディの内容を取得
+  @Post('signup')
+  signUp(@Body() dto: AuthDto): Promise<Msg> {
+    return this.authService.signUp(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(
+    @Body() dto: AuthDto,
+    // passthrough: trueは
+    // NestJSのスタンダードモードとExpressのレスポンスのオブジェクトモードを両立するために必要
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Msg> {
+    const jwt = await this.authService.login(dto);
+    // クライアント側のcookieにjwtをセット
+    res.cookie('access_token', jwt.accesseToken, {
+      httpOnly: true,
+      // httpsで暗号化された通信のみcookieを使用化（デプロイするまでfalse）
+      secure: false,
+      sameSite: 'none',
+      path: '/',
+    });
+
+    return {
+      message: 'ok',
+    };
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('logout')
+  logout(@Res() req: Request, @Res({ passthrough: true }) res: Response): Msg {
+    res.cookie('access_token', '', {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'none',
+      path: '/',
+    });
+
+    return {
+      message: 'ok',
+    };
+  }
+}
