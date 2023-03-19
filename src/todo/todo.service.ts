1c5021358b338a3063997810abe3cf178ad5e455
Kadotchi <kadotchi724@gmail.com>
2023-03-19 19:35:57 +0900

    ✨ Todo機能を実装

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from '@prisma/client';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * ログインしてるユーザーの全てのタスクを取得
+   * @param userId
+   */
+  getTasks(userId: number): Promise<Task[]> {
+    return this.prisma.task.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createAt: 'desc',
+      },
+    });
+  }
+
+  getTaskById(userId: number, taskId: number): Promise<Task> {
+    return this.prisma.task.findFirst({
+      where: {
+        userId,
+        id: taskId,
+      },
+    });
+  }
+
+  async createTask(userId: number, dto: CreateTaskDto): Promise<Task> {
+    const task = await this.prisma.task.create({
+      data: {
+        userId,
+        ...dto,
+      },
+    });
+    return task;
+  }
+
+  /**
+   * タスクの更新
+   * @param userId ログインしてるユーザーのID
+   * @param taskId 更新対象のタスクのID
+   * @param dto 更新したい内容
+   * @returns
+   */
+  async updateTaskById(
+    userId: number,
+    taskId: number,
+    dto: UpdateTaskDto,
+  ): Promise<Task> {
+    const task = await this.prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    // ログインしてるユーザーのタスクじゃない場合はエラー
+    if (!task || task.userId !== userId) {
+      throw new ForbiddenException('No permission to update');
+    }
+
+    return this.prisma.task.update({
+      where: {
+        id: taskId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteTaskById(userId: number, taskId: number): Promise<void> {
+    const task = await this.prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    if (!task || task.userId !== userId)
+      throw new ForbiddenException('No permission to delete');
+
+    await this.prisma.task.delete({
+      where: {
+        id: taskId,
+      },
+    });
+  }
+}
